e95f8ca2eddc212e42eda6bb4f0ccd4809caf338
皓霄 <xhao24555@gmail.com>
2026-06-20 20:33:33 +0800

    Update qwqui1.lua

--- qwqui1.lua
+++ qwqui1.lua
@@ -5530,7 +5530,7 @@ function Library:CreateWindow(Config)
 	end
 
 	Library.MinimizeKey = Config.MinimizeKey or Enum.KeyCode.LeftControl
-	Library.Theme = "SoftCream"
+	Library.Theme = "Sunset"
 
 	local Window = Components.Window({
 		Parent = GUI,
