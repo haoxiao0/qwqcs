0ba4c57f066825fa6216307a926b0a5a94657c31
皓霄 <xhao24555@gmail.com>
2026-06-24 18:52:44 +0800

    Update qwq.lua

--- qwq.lua
+++ qwq.lua
@@ -4,7 +4,7 @@ local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.
 
 local Window = Fluent:CreateWindow({
     Title = "QWQ",
-    SubTitle = "3.3",
+    SubTitle = "3.8",
     TabWidth = 100,
     Size = UDim2.fromOffset(450, 350),
     Acrylic = true,
