058187b791151a0ca371ddde2cc967acebcea0c3
iwajjvguh <xhao24555@gmail.com>
2026-07-18 23:44:59 +0800

    Update window subtitle in qwq5.0.lua

--- qwq5.0.lua
+++ qwq5.0.lua
@@ -4,7 +4,7 @@ local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.
 
 local Window = Fluent:CreateWindow({
     Title = "QWQ",
-    SubTitle = "7.2(感谢使用~)(更新了防甩飞)",
+    SubTitle = "7.2",
     TabWidth = 100,
     Size = UDim2.fromOffset(450, 350),
     Acrylic = true,
